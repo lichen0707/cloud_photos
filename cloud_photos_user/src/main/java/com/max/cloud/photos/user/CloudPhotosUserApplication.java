8eb7f6cf83286d4b95144188a937bf67993a9426
lichen0707 <314754486@qq.com>
2024-09-19 14:28:26 +0800

    Update .gitignore

---
+++ cloud_photos_user/src/main/java/com/max/cloud/photos/user/CloudPhotosUserApplication.java
@@ -0,0 +1,13 @@
+package com.max.cloud.photos.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPhotosUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPhotosUserApplication.class, args);
+    }
+
+}
---
+++ cloud_photos_user/src/test/java/com/max/cloud/photos/user/CloudPhotosUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.cloud.photos.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPhotosUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
